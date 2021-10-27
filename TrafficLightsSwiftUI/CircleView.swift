30024e87ba7190ac316aab9e9aa4d0069da8e9c0
Ilya Nechaev <dolphins.mind777@gmail.com>
2021-10-27 21:23:13 +0300

    build interface

---
+++ TrafficLightsSwiftUI/CircleView.swift
@@ -0,0 +1,28 @@
+//
+//  CircleView.swift
+//  TrafficLightsSwiftUI
+//
+//  Created by Илья Нечаев on 26.10.2021.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    let color: Color
+    @State var alpha: Double
+    
+    var body: some View {
+        Circle()
+            .foregroundColor(color)
+            .opacity(alpha)
+            .frame(width: 150, height: 150)
+            .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            .shadow(color: .black, radius: 10)
+    }
+}
+
+struct CircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleView(color: .red, alpha: 1)
+    }
+}
--- TrafficLightsSwiftUI/ContentView.swift
+++ TrafficLightsSwiftUI/ContentView.swift
@@ -7,12 +7,76 @@
 
 import SwiftUI
 
+enum ThreeColor {
+    case red, green, yellow
+}
+
 struct ContentView: View {
+    @State private var currentColor: ThreeColor = .green
+    @State private var onRed: Bool = false
+    @State private var onYellow: Bool = false
+    @State private var onGreen: Bool = false
+    
+    @State private var wasPressed = false
+    private let offLight: Double = 0.3
+    private let onLight: Double = 1
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.purple, .pink]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomLeading)
+                .edgesIgnoringSafeArea(.all )
+            
+            VStack {
+                
+                VStack(spacing: 18) {
+                    CircleView(color: .red, alpha: onRed ? onLight : offLight)
+                    CircleView(color: .yellow, alpha: onYellow ? onLight : offLight)
+                    CircleView(color: .green, alpha: onGreen ? onLight : offLight)
+                }
+                .padding()
+                
+                Spacer()
+                
+                Button (action: {
+                    self.wasPressed = true
+                    
+                    switch self.currentColor {
+                    case .red:
+                        onRed.toggle()
+                        onYellow.toggle()
+                        currentColor = .yellow
+                    case .green:
+                        self.onGreen.toggle()
+                        self.onRed.toggle()
+                        self.currentColor = .red
+                    case .yellow:
+                        onYellow.toggle()
+                        onGreen.toggle()
+                        currentColor = .green
+                    }
+                }) {
+                    wasPressed ? Text("NEXT") : Text("START")
+                }
+                .font(.system(size: 40, weight: .medium, design: .default).italic())
+                .foregroundColor(.black)
+                .frame(width: 200, height: 80)
+                .background(Color.white)
+                .cornerRadius(15)
+                .padding()
+                
+                Spacer()
+            }
+            
+        }
+        
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
